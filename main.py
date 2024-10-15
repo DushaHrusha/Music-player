2c97c4ea185f65c35ae5ec6221a0a8694074e495
Andey Nikitin <hrushadusha@gmail.com>
2024-10-15 16:27:23 +0600

    v2

--- main.py
+++ main.py
@@ -1,104 +1,149 @@
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
-from MusicWindow import Ui_MainWindow
-from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
-from PySide6.QtCore import QUrl, QTime
-from PySide6.QtGui import QIcon
+import os.path
+import time
+
+from PyQt5 import QtMultimedia
+from PyQt5.QtCore import QUrl, QTimer
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtWidgets import *
+from music import Ui_SimpleMusiPlayer
 
 
-class MainWindow(QMainWindow):
+class SimpleMusicPlayer(QMainWindow, Ui_SimpleMusiPlayer):
     def __init__(self):
         super().__init__()
+        self.window = QMainWindow()
+        self.setupUi(self)
+        self.show()
 
-        self.muted = True
+        # Inits
+        self.current_songs = []
+        self.current_volume = 10
 
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.setWindowTitle('Music Player Application')
+        global stopped
+        stopped = False
 
-        self.ui.toolButtonPlay.setEnabled(False)
+        self.player = QtMultimedia.QMediaPlayer()
+        self.player.setVolume(self.current_volume)
 
-        self.player = QMediaPlayer()
-        self.audio = QAudioOutput()
+        # Slider Timer
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.move_slider)
 
-        self.audioVolumeLevel = 70
-        self.player.setAudioOutput(self.audio)
-        self.audio.setVolume(self.audioVolumeLevel/100)
+        # Connections
+        self.musicSlider.sliderMoved[int].connect(lambda: self.player.setPosition(self.musicSlider.value()))
+        self.volumeSlider.sliderMoved[int].connect(lambda: self.volume_changed())
+        self.actionAdd_Songs.triggered.connect(self.add_songs)
+        self.actionRemove_Selected.triggered.connect(self.remove_one_song)
+        self.actionRemove_All.triggered.connect(self.remove_all_songs)
+        self.playpushButton.clicked.connect(self.play_song)
+        self.pausepushButton.clicked.connect(self.pause_and_unpause)
+        self.nextpushButton.clicked.connect(self.next_song)
+        self.previouspushButton.clicked.connect(self.previous_song)
+        self.stoppushButton.clicked.connect(self.stop_song)
 
+    def move_slider(self):
+        if stopped:
+            return
+        else:
+            # Update the slider
+            if self.player.state() == QMediaPlayer.PlayingState:
+                self.musicSlider.setMinimum(0)
+                self.musicSlider.setMaximum(self.player.duration())
+                slider_position = self.player.position()
+                self.musicSlider.setValue(slider_position)
 
-        self.ui.actionOpen_Music.triggered.connect(self.open_music)
-        self.ui.toolButtonPlay.clicked.connect(self.play_music)
-        self.ui.horizontalSliderVolume.sliderMoved.connect(self.volume_slider_changed)
-        self.ui.horizontalSliderVolume.setValue(self.audioVolumeLevel)
-        self.ui.horizontalSliderPlay.sliderMoved.connect(self.play_slider_changed)
-        self.ui.toolButtonPause.clicked.connect(self.pause_btn)
-        self.ui.toolButtonStop.clicked.connect(self.stop_btn)
-        self.ui.toolButtonVolume.clicked.connect(self.volume_mute)
+                current_time = time.strftime('%H:%M:%S', time.localtime(self.player.position() / 1000))
+                song_duration = time.strftime('%H:%M:%S', time.localtime(self.player.duration() / 1000))
+                self.start_time_label.setText(f"{current_time}")
+                self.end_time_label.setText(f"{song_duration}")
 
-        #connect media player signals
-        self.player.positionChanged.connect(self.position_changed)
-        self.player.durationChanged.connect(self.duration_changed)
+    def add_songs(self):
+        files, _ = QFileDialog.getOpenFileNames(
+            self, caption='Add Songs',
+            directory=':\\', filter="Supported Files (*.mp3;*.mpeg;*.ogg;*.m4a;*.MP3;*.wma;*.acc;*.amr)"
+        )
+        if files:
+            for file in files:
+                self.current_songs.append(file)
+                self.listWidget.addItem(os.path.basename(file))
 
+    def play_song(self):
+        try:
+            global stopped
+            stopped = False
 
-    def open_music(self):
-        fileName, _ = QFileDialog.getOpenFileName(self, "Open Music")
+            current_selection = self.listWidget.currentRow()
+            current_song = self.current_songs[current_selection]
 
-        if fileName != '':
-            self.player.setSource(QUrl.fromLocalFile(fileName))
-            self.ui.toolButtonPlay.setEnabled(True)
+            song_url = QMediaContent(QUrl.fromLocalFile(current_song))
+            self.player.setMedia(song_url)
+            self.player.play()
+            self.move_slider()
+        except Exception as e:
+            print(f"Play song error: {e}")
 
-    def play_music(self):
-        print('Called play music')
-        if self.player.mediaStatus == QMediaPlayer.PlaybackState.PlayingState:
+    def pause_and_unpause(self):
+        if self.player.state() == QMediaPlayer.PlayingState:
             self.player.pause()
         else:
             self.player.play()
 
-    def position_changed(self, position):
-        if(self.ui.horizontalSliderPlay.maximum() != self.player.duration()):
-            self.ui.horizontalSliderPlay.setMaximum(self.player.duration())
+    def next_song(self):
+        try:
+            current_selection = self.listWidget.currentRow()
 
-        self.ui.horizontalSliderPlay.setValue(position)
+            if current_selection + 1 == len(self.current_songs):
+                next_index = 0
+            else:
+                next_index = current_selection + 1
+            current_song = self.current_songs[next_index]
+            self.listWidget.setCurrentRow(next_index)
+            song_url = QMediaContent(QUrl.fromLocalFile(current_song))
+            self.player.setMedia(song_url)
+            self.player.play()
+            self.move_slider()
+        except Exception as e:
+            print(f"Next song error: {e}")
 
+    def previous_song(self):
+        try:
+            current_selection = self.listWidget.currentRow()
 
-        seconds = (position / 1000) % 60
-        minutes = (position / 60000) % 60
-        hours = (position / 2600000) % 24
+            if current_selection == 0:
+                previous_index = len(self.current_songs) - 1
+            else:
+                previous_index = current_selection - 1
 
-        time = QTime(hours, minutes, seconds)
-        self.ui.labelTimer.setText(time.toString())
+            current_song = self.current_songs[previous_index]
+            self.listWidget.setCurrentRow(previous_index)
+            song_url = QMediaContent(QUrl.fromLocalFile(current_song))
+            self.player.setMedia(song_url)
+            self.player.play()
+            self.move_slider()
+        except Exception as e:
+            print(f"Previous song error: {e}")
 
-
-    def duration_changed(self, duration):
-        self.ui.horizontalSliderPlay.setRange(0, duration)
-
-
-    def volume_slider_changed(self, position):
-        self.audioVolumeLevel = position
-        self.audio.setVolume(position/100)
-
-    def play_slider_changed(self, position):
-        self.player.setPosition(position)
-
-    def pause_btn(self):
-        self.player.pause()
-    def stop_btn(self):
+    def stop_song(self):
         self.player.stop()
+        self.musicSlider.setValue(0)
+        self.start_time_label.setText(f"00:00:00")
+        self.end_time_label.setText(f"00:00:00")
 
-    def volume_mute(self):
-        if(self.muted):
-            self.audio.setMuted(True)
-            self.ui.horizontalSliderVolume.setValue(0)
-            self.ui.toolButtonVolume.setIcon(QIcon(":/icons/mute.png"))
-            self.muted = False
-        else:
-            self.audio.setMuted(False)
-            self.ui.horizontalSliderVolume.setValue(self.audioVolumeLevel)
-            self.ui.toolButtonVolume.setIcon(QIcon(":/icons/volume.png"))
-            self.muted = True
+    def volume_changed(self):
+        try:
+            self.current_volume = self.volumeSlider.value()
+            self.player.setVolume(self.current_volume)
+            self.volume_label.setText(f"{self.current_volume}")
+        except Exception as e:
+            print(f"Changing volume error: {e}")
 
+    def remove_one_song(self):
+        current_selection = self.listWidget.currentRow()
+        self.current_songs.pop(current_selection)
+        self.listWidget.takeItem(current_selection)
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-sys.exit(app.exec())
+    def remove_all_songs(self):
+        self.stop_song()
+        self.listWidget.clear()
+        self.current_songs.clear()
